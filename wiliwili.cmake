7c727db41a2e42224d979dea0080b2a51d11a15a
ikas <65895428+ikas-mc@users.noreply.github.com>
2024-01-01 00:21:58 +0800

    add source group

--- wiliwili.cmake
+++ wiliwili.cmake
@@ -15,7 +15,6 @@ file(GLOB_RECURSE PACKAGE_ASSETS_FILES "${CMAKE_CURRENT_SOURCE_DIR}/wiliwili-uwp
 set_property(SOURCE ${PACKAGE_ASSETS_FILES} PROPERTY VS_DEPLOYMENT_CONTENT 1)
 set_property(SOURCE ${PACKAGE_ASSETS_FILES} PROPERTY VS_DEPLOYMENT_LOCATION "Assets")
 
-
 # resource
 set(PROJECT_RESOURCES "${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/resources")
 
@@ -32,18 +31,25 @@ file(GLOB_RECURSE PROJECT_RESOURCES_FILES "${PROJECT_RESOURCES}/*")
 foreach(FILE IN LISTS PROJECT_RESOURCES_FILES)
     add_deployment_content("${PROJECT_RESOURCES}/.." "${FILE}")
 endforeach()
+source_group(TREE "${PROJECT_RESOURCES}" PREFIX "resource" FILES ${PROJECT_RESOURCES_FILES})
 
 # source 
-set(APP_PLATFORM_INCLUDE)
+set(HEADER_INCLUDES)
 
 file(GLOB_RECURSE MAIN_SRC "${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/wiliwili/source/*.cpp")
+list(APPEND HEADER_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/wiliwili/include)
+list(APPEND HEADER_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/wiliwili/include/api)
+
 file(GLOB_RECURSE PDR_SRC ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/libpdr/src/*.cpp)
+list(APPEND HEADER_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/libpdr/include)
 
 file(GLOB_RECURSE PYSTRING_SRC ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/pystring/*.cpp)
-list(APPEND APP_PLATFORM_INCLUDE ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/pystring)
+list(APPEND HEADER_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/pystring)
+
+source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/wiliwili" PREFIX "sources" FILES ${MAIN_SRC} ${PDR_SRC} ${PYSTRING_SRC})
 
 # Target
-add_executable(${PROJECT_NAME} WIN32 ${MAIN_SRC} ${PDR_SRC}  ${PYSTRING_SRC} ${TEMPLATE_FILES} ${PROJECT_RESOURCES_FILES} ${PACKAGE_ASSETS_FILES})
+add_executable(${PROJECT_NAME} WIN32 ${MAIN_SRC} ${PDR_SRC} ${PYSTRING_SRC} ${TEMPLATE_FILES} ${PROJECT_RESOURCES_FILES} ${PACKAGE_ASSETS_FILES})
 
 find_package(cppwinrt CONFIG REQUIRED)
 target_link_libraries(${PROJECT_NAME} Microsoft::CppWinRT)
@@ -61,7 +67,7 @@ target_link_libraries(${PROJECT_NAME} unofficial::nayuki-qr-code-generator::nayu
 # find_package(pystring CONFIG REQUIRED)
 # target_link_libraries(${PROJECT_NAME} pystring::pystring)
 # find_path(PYSTRING_INCLUDE_DIR pystring.h PATH_SUFFIXES pystring REQUIRED)
-# list(APPEND APP_PLATFORM_INCLUDE ${PYSTRING_INCLUDE_DIR})
+# list(APPEND HEADER_INCLUDES ${PYSTRING_INCLUDE_DIR})
 
 find_package(OpenSSL REQUIRED)
 target_link_libraries(${PROJECT_NAME} OpenSSL::SSL OpenSSL::Crypto)
@@ -82,6 +88,6 @@ target_link_libraries(${PROJECT_NAME} cpr::cpr)
 
 target_link_libraries(${PROJECT_NAME} borealis)
 
-target_include_directories(${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/wiliwili/include ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/wiliwili/include/api ${CMAKE_CURRENT_SOURCE_DIR}/wiliwili/library/libpdr/include ${APP_PLATFORM_INCLUDE} )
+target_include_directories(${PROJECT_NAME} PUBLIC  ${HEADER_INCLUDES} )
 target_compile_options(${PROJECT_NAME} PRIVATE -DBRLS_RESOURCES_DIR=".")
-target_compile_definitions(${PROJECT_NAME} PRIVATE  BOREALIS_USE_STD_THREAD __WINRT__ BOREALIS_USE_D3D11 __SDL2__ NOMINMAX _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS _WINSOCK_DEPRECATED_NO_WARNINGS _CRT_SECURE_NO_WARNINGS _CRT_NONSTDC_NO_WARNINGS)
+target_compile_definitions(${PROJECT_NAME} PRIVATE  BOREALIS_USE_STD_THREAD __WINRT__ BOREALIS_USE_D3D11 __SDL2__ __PLAYER_WINRT__ NOMINMAX _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS _WINSOCK_DEPRECATED_NO_WARNINGS _CRT_SECURE_NO_WARNINGS _CRT_NONSTDC_NO_WARNINGS)
